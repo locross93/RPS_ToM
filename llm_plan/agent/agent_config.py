61317a754981e821cf59c5eb7bc931a68ec58716
locross93 <locross93@gmail.com>
2025-02-17 20:57:33 -0800

    Choose v3

--- llm_plan/agent/agent_config.py
+++ llm_plan/agent/agent_config.py
@@ -2,6 +2,7 @@ agent_config = {
     'hm': 'llm_plan.agent.rps.rps_hypothetical_minds.DecentralizedAgent',
     'give_hypothesis': 'llm_plan.agent.rps.hm_give_hypothesis.DecentralizedAgent',
     'choose_hypothesis': 'llm_plan.agent.rps.hm_choose_hypothesis.DecentralizedAgent',
+    'choose_hyp': 'llm_plan.agent.rps.hm_choose_hyp.DecentralizedAgent',
     'react': 'llm_plan.agent.rps.react.DecentralizedAgent',
     'base_llm': 'llm_plan.agent.rps.base_llm.DecentralizedAgent',
     'hm_mod_memory': 'llm_plan.agent.rps.hm_mod_memory.DecentralizedAgent',
